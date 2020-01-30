735df873e0594ac8b8b5e34085529647883c5f55
Mira <myrahosd@hotmail.fr>
2020-01-30 21:37:06 +0100

    Itération 1

---
+++ starter-code/src/Greetings.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Greetings extends Component {
+  render() {
+    return (
+    greetings = {
+        lang: String,
+        children: Text,
+    }
+    );
+  }
+}
+
+export default Greetings;
---
+++ starter-code/src/IdCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class IdCard extends Component {
+  render() {
+    return (
+    user = {
+        lastName: String,
+        firstName: String,
+        gender: String,
+        height: Number,
+        birth: Date,
+        picture: String
+    }
+    );
+  }
+}
+
+export default IdCard;
